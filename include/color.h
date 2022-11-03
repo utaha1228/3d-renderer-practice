31eb74878ecb50e22f9a8c2df08dcb2506f9ed03
Yen-Jen Wang <wangyenjen0301@gmail.com>
2022-11-03 09:19:33 +0800

    Add const.h.

--- include/color.h
+++ include/color.h
@@ -28,11 +28,3 @@ struct RGB {
 
 	const RGB copy() const;
 };
-
-#define WHITE RGB(255, 255, 255)
-#define BLACK RGB(0, 0, 0)
-#define RED RGB(255, 0, 0)
-#define GREEN RGB(0, 255, 0)
-#define BLUE RGB(0, 0, 255)
-#define YELLOW RGB(255, 255, 0)
-#define PURPLE RGB(127, 0, 255)
---
+++ include/const.h
@@ -0,0 +1,9 @@
+#define PI 3.14159265358979323
+
+#define WHITE RGB(255, 255, 255)
+#define BLACK RGB(0, 0, 0)
+#define RED RGB(255, 0, 0)
+#define GREEN RGB(0, 255, 0)
+#define BLUE RGB(0, 0, 255)
+#define YELLOW RGB(255, 255, 0)
+#define PURPLE RGB(127, 0, 255)
